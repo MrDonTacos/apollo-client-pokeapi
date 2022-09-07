17ff2e6f8f0370ed2e58da0998bbfb4f3dcadfbb
Saúl Cañedo Castillo <saul.canedocast@hotmail.com>
2022-09-07 10:08:25 -0700

    add apollo configutarion

---
+++ src/apollo/client.js
@@ -0,0 +1,12 @@
+import fetch from 'isomorphic-fetch';
+import {ApolloClient, HttpLink, InMemoryCache} from '@apollo/client';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:4000',
+    fetch
+  })
+});
+
+export default client;
