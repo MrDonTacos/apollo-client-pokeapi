81c1f62dacf68ae3c1e533dcbcffbab4993ebaa2
Saúl Cañedo Castillo <saul.canedocast@hotmail.com>
2022-09-12 11:30:21 -0700

    chore: add login route

--- src/pages/app/[...].js
+++ src/pages/app/[...].js
@@ -3,12 +3,14 @@ import Layout from '../../components/layout'
 import {Router} from '@reach/router'
 import CardList from "../../components/cards";
 import Pokemon from "../../components/pokemon";
+import Login from "../../components/login";
 const App = () => {
     return (
         <Layout>
             <Router basepath="/app">
                 <CardList path="/" />
-                <Pokemon path="/:name" />
+                <Pokemon path="/pokemon/:name" />
+                <Login path="/login" />
             </Router>
         </Layout>
     )
